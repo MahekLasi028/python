f5c8722fab40fe7aa00bc7906514b2036c2f2352
MahekLasi028 <maheklasi@gmail.com>
2026-07-05 16:13:28 +0530

    33th

---
+++ AddLamda.py
@@ -0,0 +1,9 @@
+Addition = lambda no1 , no2 :no1 + no2
+
+def main():
+    num1 = int(input("Enter 1 st no :"))
+    num2 = int(input("Enter 2nd no :"))
+    print(Addition(num1, num2))
+
+if __name__ == "__main__":
+    main()
---
+++ Divisible.py
@@ -0,0 +1,8 @@
+Divisible = lambda n : n % 5 ==0 
+
+def main():
+    num = int(input("Enter no :"))
+    print(Divisible(num))
+
+if __name__ == "__main__":
+    main()
