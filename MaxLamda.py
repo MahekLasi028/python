72f6068c1b15b6d8d79c88c1352cbd8502886f6b
MahekLasi028 <maheklasi@gmail.com>
2026-07-05 15:58:30 +0530

    28th

---
+++ MaxLamda.py
@@ -0,0 +1,10 @@
+max = lambda no1 , no2 : no1 if no1 >no2 else no2
+
+def main():
+    n1 = int(input("Enter 1st no :"))
+    n2 = int(input("Enter 2nd no :"))
+
+    print("Max : ",max(n1,n2))
+
+if __name__ == "__main__":
+    main()
