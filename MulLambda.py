d5288d579a28bee3eee57566aaa91d4fdbd0fa69
MahekLasi028 <maheklasi@gmail.com>
2026-07-05 16:15:32 +0530

    34th

---
+++ MulLambda.py
@@ -0,0 +1,9 @@
+Mul = lambda no1 , no2 :no1 * no2
+
+def main():
+    num1 = int(input("Enter 1 st no :"))
+    num2 = int(input("Enter 2nd no :"))
+    print(Mul(num1, num2))
+
+if __name__ == "__main__":
+    main()
