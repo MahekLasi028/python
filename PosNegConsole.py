b802953c0cd0ab71e748204ec6eba11e6657a902
MahekLasi028 <maheklasi@gmail.com>
2026-07-11 11:16:06 +0530

    51h console program

--- PosNegConsole.py
+++ PosNegConsole.py
@@ -1,15 +1,19 @@
 def ChkNum(no):
 
-    if no>=0:
+    if no>0:
         return True
+    elif no==0:
+        return "Zero"
     else:
         return False
 
 def main():
 
     Ret = int(input("Enter  number  :"))
-    if(ChkNum(Ret)):
+    if ChkNum(Ret) == True:
         print("Positive")
+    elif ChkNum(Ret) == "Zero":
+        print("Zero")
     else:
         print("Negative")
     
