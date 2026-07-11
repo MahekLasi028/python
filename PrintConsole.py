57bee21926771b76824c1223cb240ccd01061b10
MahekLasi028 <maheklasi@gmail.com>
2026-07-11 11:31:26 +0530

    53rd console program

---
+++ PrintConsole.py
@@ -0,0 +1,12 @@
+def PrintNo(no):
+
+    for i in range(1,no+1):
+        print("*")
+
+def main():
+
+    No = int(input("Enter  number  :"))
+    PrintNo(No)
+    
+if __name__ == "__main__":
+    main()
