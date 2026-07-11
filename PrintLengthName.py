56089256431b9e4389cb71b3251ec9789fda1518
MahekLasi028 <maheklasi@gmail.com>
2026-07-11 11:39:55 +0530

    55th console program

---
+++ PrintLengthName.py
@@ -0,0 +1,11 @@
+def PrintLength(name):
+
+    print("Length of name is : ",len(name))
+
+def main():
+
+    Name = input("Enter  name  :")
+    PrintLength(Name)
+    
+if __name__ == "__main__":
+    main()
