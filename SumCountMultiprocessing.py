f9d5d2aa7a596940072161ec9320b48484f57148
MahekLasi028 <maheklasi@gmail.com>
2026-07-12 23:24:21 +0530

    67th..

---
+++ SumCountMultiprocessing.py
@@ -0,0 +1,28 @@
+from multiprocessing import Pool
+import os
+
+def CountEven(no):
+    count = 0
+
+    for i in range(2, no + 1, 2):
+        count = count + 1
+
+    print("Process ID :", os.getpid())
+    print("Input Number :", no)
+    print("Even Number Count :", count)
+    print()
+
+    return count
+
+def main():
+    data = [1000000, 2000000, 3000000, 4000000]
+
+    p = Pool()
+
+    result = p.map(CountEven, data)
+
+    p.close()
+    p.join()
+
+if __name__ == "__main__":
+    main()
