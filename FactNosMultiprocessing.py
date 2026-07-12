be8a9ccdb7258175b7fd19ed77fca776647059dd
MahekLasi028 <maheklasi@gmail.com>
2026-07-12 23:31:57 +0530

    69th..

---
+++ FactNosMultiprocessing.py
@@ -0,0 +1,28 @@
+from multiprocessing import Pool
+import os
+
+def Factorial(no):
+    fact = 1
+
+    for i in range(1, no + 1):
+        fact = fact * i
+
+    print("Process ID :", os.getpid())
+    print("Input Number :", no)
+    print("Factorial :", fact)
+    print()
+
+    return fact
+
+def main():
+    data = [10, 15, 20, 25]
+
+    p = Pool()
+
+    result = p.map(Factorial, data)
+
+    p.close()
+    p.join()
+
+if __name__ == "__main__":
+    main()
