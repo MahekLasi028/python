c6f92d7971c289b08830a2d88f3ed298144cdf77
MahekLasi028 <maheklasi@gmail.com>
2026-07-12 16:04:35 +0530

    63th

---
+++ CountPrimeMultiprocessing.py
@@ -0,0 +1,36 @@
+from multiprocessing import Pool
+
+def isPrime(no):
+    if no<=1:
+        return False
+    
+    for i in range(2,no):
+        if no % i == 0:
+            return False
+        
+    return True
+
+def CountPrime(no):
+    count = 0
+
+    for i in range(2,no+1):
+        if isPrime(i):
+            count += 1
+
+    return count
+
+def main():
+    data = [10000,20000,30000,40000]
+
+    p = Pool()
+
+    result = p.map(CountPrime,data)
+
+    p.close()
+    p.join()
+
+if __name__ =="__main__":
+    main
+
+
+        
